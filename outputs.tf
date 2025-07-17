37fb98017153ced7dcf5e0fc464e95c5501dfba4
sudha <Sudhaas2802@gmail.com>
2025-07-17 15:11:51 -0400

    vpc module

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,6 @@
-output "azs_info" {
-    value = data.aws_availability_zones.available
-}
+# output "azs_info" {
+#     value = data.aws_availability_zones.available
+# }
+# output "subnets_info" {
+#     value = aws_subnet.public
+# }
--- variables.tf
+++ variables.tf
@@ -1,25 +1,70 @@
 variable "vpc_cidr" {
 
 }
-variable "enable_dns_hostnames" {
-    default = true
-}
-variable "common_tags" {
-
-}
-variable "vpc_tags" {
-    default = {}
-}
-variable "project" {
+variable "project_name"{
 
 }
 variable "environment" {
 
 }
+variable "enable_dns_hostnames" {
+    default = true
+}
+variable "common_tags" {
+    type = map
+}
+variable "vpc_tags" {
+    default = {}
+} 
+variable "igw_tags" {
+   default = {}
+}  
 variable "public_subnet_cidrs" {
     type = list
     validation {
-      condition     = length(var.public_subnet_cidrs) == 2
-      error_message = "please provide 2 subnets"
-    }
- }
+       condition     = length(var.public_subnet_cidrs) == 2
+       error_message = "please provide public subnet cidrs"
+  } 
+} 
+variable "public_subnet_tags" {
+    default = {}
+}
+variable "private_subnet_cidrs" {
+    type = list
+    validation {
+       condition     = length(var.private_subnet_cidrs) == 2
+       error_message = "please provide private subnet cidrs"
+  } 
+} 
+variable "private_subnet_tags" {
+    default = {}
+}
+variable "database_subnet_cidrs" {
+    type = list
+    validation {
+       condition     = length(var.database_subnet_cidrs) == 2
+       error_message = "please provide database subnet cidrs"
+  } 
+} 
+variable "database_subnet_tags" {
+    default = {}
+}
+variable "nat_gateway_tags" {
+    default = {}
+}
+variable "public_route_table_tags" {
+    default = {}
+}
+variable "private_route_table_tags" {
+    default = {}
+}
+variable "database_route_table_tags" {
+    default = {}
+}
+  
+variable "is_peering_required" {
+    default = false
+}
+variable "vpc_peering_tags" {
+    default = {}
+}
